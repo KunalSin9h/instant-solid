12e81a67fe439e3d782664ade94947830a90fd63
Kunal Singh <kunalsin9h@gmail.com>
2024-09-01 23:33:23 +0530

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import solid from 'vite-plugin-solid'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [solid()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'instant-solid',
+      formats: ['es', 'umd'],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['solid-js'],
+      output: {
+        globals: {
+          'solid-js': 'Solid',
+        },
+      },
+    },
+  },
+})
+
