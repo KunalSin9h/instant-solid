fcada87c92cec73ee1cd1a05092c8af3165cdb57
Kunal Singh <kunalsin9h@gmail.com>
2024-09-02 08:19:45 +0530

    wip

--- src/InstantSolid.ts
+++ src/InstantSolid.ts
@@ -18,6 +18,8 @@ import {
 import { useQuery } from "./useQuery";
 import { useTimeout } from "./useTimeout";
 
+import { createSignal, createEffect, createMemo, onCleanup } from "solid-js";
+
 export type PresenceHandle<
     PresenceShape,
     Keys extends keyof PresenceShape
@@ -62,4 +64,78 @@ export class InstantSolidRoom<
         this.id = id;
     }
 
+    /**
+    * Listen for broadcasted events given a room and topic.
+    *
+    * @see https://instantdb.com/docs/presence-and-topics
+    * @example
+    *  function App({ roomId }) {
+    *    db.room(roomType, roomId).useTopicEffect("chat", (message, peer) => {
+    *      console.log("New message", message, 'from', peer.name);
+    *    });
+    *   
+    *    // ...
+    *  }
+    */
+   createTopicEffect = <TopicType extends keyof RoomSchema[RoomType]["topics"]>(
+        topic: TopicType,
+        onEvent: (
+            event: RoomSchema[RoomType]["topics"][TopicType],
+            peer: RoomSchema[RoomType]["presence"]
+        ) => void
+    ): void => {
+        const [roomId] = createSignal(this.id);
+        const [roomTopic] = createSignal(topic);
+
+        createEffect(() => {
+            const unsubscribe = this._core._reactor.subscribeTopic(
+                roomId(),
+                roomTopic(),
+                (event, peer) => {
+                    onEvent(event, peer);
+                }
+            );
+
+            onCleanup(() => {
+                unsubscribe();
+            });
+        });
+    }
+    
+
+    /**
+     * Broadcast an event to a room.
+     *
+     * @see https://instantdb.com/docs/presence-and-topics
+     * @example
+     * function App({ roomId }) {
+     *   const publishTopic = db.room(roomType, roomId).createPublishTopic("clicks");
+     *
+     *   return (
+     *     <button onClick={() => publishTopic({ ts: Date.now() })}>Click me</button>
+     *   );
+     * }
+     *
+     */
+    createPublishTopic = <Topic extends keyof RoomSchema[RoomType]["topics"]>(
+        topic: Topic
+    ): ((data: RoomSchema[RoomType]["topics"][Topic]) => void) => {
+        const [roomId] = createSignal(this.id);
+        const [roomTopic] = createSignal(topic);
+
+        createEffect(() => {
+            this._core._reactor.joinRoom(roomId());
+        });
+
+        const publishTopic = createMemo((data) => {
+            this._core._reactor.publishTopic({
+                roomType: this.type,
+                roomId: roomId(),
+                topic: roomTopic(),
+                data,
+            });
+        });
+
+        return publishTopic;
+    }
 }
